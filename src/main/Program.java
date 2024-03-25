b14afd9f6b00a3286cd3eead8b2662697be9fc1c
Tiago Gomes <tidgomes@gmail.com>
2024-03-25 10:06:29 -0300

    Split String data into a Map

--- src/main/Program.java
+++ src/main/Program.java
@@ -2,22 +2,32 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		try {
-			File myObj = new File("c:\\users\\tidgo\\Documentos\\teste.txt");
-			Scanner myReader = new Scanner(myObj);
-			while (myReader.hasNextLine()) {
-				String data = myReader.nextLine();
-				System.out.println(data);
+			File myFile = new File("c:\\users\\tidgo\\Documentos\\teste.txt");
+			Scanner sc = new Scanner(myFile);
+
+			Map<String, Integer> people = new HashMap<String, Integer>();
+			while (sc.hasNextLine()) {
+				String data = sc.nextLine();
+				String[] dataSplit = data.split(",");
+
+				people.put(dataSplit[0], Integer.parseInt(dataSplit[1]));
 			}
-			
-			myReader.close();
+
+			sc.close();
+			people.forEach((name, age) -> {
+				System.out.println("Name: " + name);
+				System.out.println("Age: " + age);
+				System.out.println();
+			});
 		} catch (FileNotFoundException e) {
-			System.out.println("An error occurred");
 			e.printStackTrace();
 		}
 
