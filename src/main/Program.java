a663f7e1db73936372b52bf1fcc0e2bcb29de334
Tiago Gomes <tidgomes@gmail.com>
2024-03-25 10:18:01 -0300

    Explore reading files info

--- src/main/Program.java
+++ src/main/Program.java
@@ -2,33 +2,36 @@ package main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
 		try {
-			File myFile = new File("c:\\users\\tidgo\\Documentos\\tete.txt");
+			File myFile = new File("c:\\users\\tidgo\\Documentos\\teste.txt");
 			Scanner sc = new Scanner(myFile);
 
-			Map<String, Integer> people = new HashMap<String, Integer>();
-			while (sc.hasNextLine()) {
-				String data = sc.nextLine();
-				String[] dataSplit = data.split(",");
+			if (myFile.exists()) {
+				System.out.println("File name: " + myFile.getName());
+				System.out.println("Can Read? " + myFile.canRead());
+				System.out.println("Can write? " + myFile.canWrite());
+				System.out.println("Absolute path: " + myFile.getAbsolutePath());
 
-				people.put(dataSplit[0], Integer.parseInt(dataSplit[1]));
+				try {
+					System.out.println("Absolute canonical: " + myFile.getCanonicalPath());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+				System.out.println("Parent? " + myFile.getParent());
+				System.out.println("Total Space? " + myFile.getTotalSpace());
+				System.out.println("Length? " + myFile.length());
 			}
 
 			sc.close();
-			people.forEach((name, age) -> {
-				System.out.println("Name: " + name);
-				System.out.println("Age: " + age);
-				System.out.println();
-			});
 		} catch (FileNotFoundException e) {
-			throw new ReadException("Something went wrong with the file reading fun process");
+			e.printStackTrace();
 		}
 
 	}
