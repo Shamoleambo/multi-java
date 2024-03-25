0eab63d766dbb6a0f823e2b7de6df5d6c714c211
Tiago Gomes <tidgomes@gmail.com>
2024-03-25 09:58:48 -0300

    read files using File and Scanner

---
+++ src/main/Program.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		try {
+			File myObj = new File("c:\\users\\tidgo\\Documentos\\teste.txt");
+			Scanner myReader = new Scanner(myObj);
+			while (myReader.hasNextLine()) {
+				String data = myReader.nextLine();
+				System.out.println(data);
+			}
+			
+			myReader.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred");
+			e.printStackTrace();
+		}
+
+	}
+
+}
